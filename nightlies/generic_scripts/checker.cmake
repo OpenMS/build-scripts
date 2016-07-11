011583b9784944b5f12365de0ea2c9920b278224
jpfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2016-07-11 19:47:37 +0200

    Removed Buildprefix from log name.
    For now we should use different builds. I do not know if this should ever happen in a single build environment (to have two logs).

--- nightlies/generic_scripts/checker.cmake
+++ nightlies/generic_scripts/checker.cmake
@@ -18,7 +18,7 @@ SET (CTEST_BUILD_NAME "${CTEST_BUILD_NAME}_Checker")
 # now we hack our own checker into cdash
 # we assume here that all tests have been build/run already
 MACRO (CTEST_CHECKER)
-  set(CHECKER_LOG "${CTEST_BINARY_DIRECTORY}/${OPENMS_BUILDNAME_PREFIX}_checker.log")
+  set(CHECKER_LOG "${CTEST_BINARY_DIRECTORY}/checker.log")
   safe_message("Starting checker with log in ${CHECKER_LOG}")
   
   execute_process(
