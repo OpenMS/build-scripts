67bf8847365b21de7905c1c1ba0df14eadc1d65b
jpfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2016-07-11 19:42:00 +0200

    Updated naming of checker.log

--- nightlies/generic_scripts/checker.cmake
+++ nightlies/generic_scripts/checker.cmake
@@ -18,12 +18,12 @@ SET (CTEST_BUILD_NAME "${CTEST_BUILD_NAME}_Checker")
 # now we hack our own checker into cdash
 # we assume here that all tests have been build/run already
 MACRO (CTEST_CHECKER)
-  set(CHECKER_LOG "${CTEST_BINARY_DIRECTORY}/${OPENMS_BUILDNAME_PREFIX}checker.log")
+  set(CHECKER_LOG "${CTEST_BINARY_DIRECTORY}/${OPENMS_BUILDNAME_PREFIX}_checker.log")
   safe_message("Starting checker with log in ${CHECKER_LOG}")
   
   execute_process(
     COMMAND php ${CTEST_SOURCE_DIRECTORY}/tools/checker.php ${CTEST_SOURCE_DIRECTORY} ${CTEST_BINARY_DIRECTORY} -r
-    OUTPUT_FILE ${CTEST_BINARY_DIRECTORY}/checker.log
+    OUTPUT_FILE ${CHECKER_LOG}
     WORKING_DIRECTORY ${CTEST_SOURCE_DIRECTORY}
   )
  
