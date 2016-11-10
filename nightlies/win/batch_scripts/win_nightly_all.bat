98f14855062d11ea3d19f8fec9410320309eaf98
Julianus Pfeuffer <jpfeuffer@fu-berlin.de>
2016-11-10 18:47:56 +0100

    Complete rework of scripts. Removing unnecessary cmake scripts. Adding setup scripts for platforms.

--- nightlies/win/batch_scripts/win_nightly_all.bat
+++
@@ -1,8 +0,0 @@
-REM call nightly test builds for win8
-set LOG_PATH=C:\dev\NIGHTLY\logs
-
-REM call test drivers for the individual VS + ARCH combinations
-call win_nightly_VS10_x64.bat > %LOG_PATH%\win_nightly_VS10_x64_host.log
-REM call win_nightly_VS10_x86.bat > %LOG_PATH%\win_nightly_VS10_x86_host.log
-REM call win_nightly_VS12_x64.bat > %LOG_PATH%\win_nightly_VS12_x64_host.log
-call win_nightly_VS12_x86.bat > %LOG_PATH%\win_nightly_VS12_x86_host.log
--- packaging/win/FTP_PATH.bat
+++
@@ -1,5 +0,0 @@
-REM rsync setup
-set HOME=c:\dev\KNIME_PACKAGE\rsync\home
-set TARGET=jpfeuffer@knecht.imp.fu-berlin.de:/web/ftp.imp.fu-berlin.de/pub/OpenMS/nightly_binaries/
-set RSYNC=C:\dev\KNIME_PACKAGE\rsync\rsync.exe
-set SSH=C:\dev\KNIME_PACKAGE\rsync\ssh.exe
