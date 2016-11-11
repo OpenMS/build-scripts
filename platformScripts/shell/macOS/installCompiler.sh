8b8a85b8c14d3f1cf5e38b92e95f7d90d2f5d42f
Julianus Pfeuffer <jpfeuffer@fu-berlin.de>
2016-11-11 16:14:30 +0100

    Forgot file

---
+++ platformScripts/shell/macOS/installCompiler.sh
@@ -0,0 +1,25 @@
+if [[ $1 ~= ^appleclang*$ ]]
+  then
+  source installCommandlineTools.sh
+elif [[ $1 ~= ^g++*$ ]]
+    then
+    formulaname=${1/+/c}
+    brew tap homebrew/versions
+    brew install $formulaname
+    if [[ -z $($1 -dumpversion) ]]
+    then
+      export COMPILER_ID="g++-$(g++ -dumpversion)"
+      export CXX=$(which $1)
+      export CC=$(which $formulaname)
+      echo "Installed $COMPILER_ID"
+    else
+      echo "Compiler installation failed. Check package name, repo settings/availability and the script $0."    
+    fi
+elif [[ $1 ~= ^clang*$ ]]
+    echo "Not supported yet"
+    exit 1
+else
+    echo "Unsupported compiler on macOS. Aborting."
+    exit 1
+fi
+    
