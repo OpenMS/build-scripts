5ba8dfeed79a2461285ed4f7a2229c9c01c4238f
Julianus Pfeuffer <jpfeuffer@fu-berlin.de>
2016-11-15 14:20:50 +0100

    weird

--- platformScripts/shell/windows/installQT.sh
+++ platformScripts/shell/windows/installQT.sh
@@ -4,6 +4,7 @@ if [[ -z $QT_VERSIONS_PATH ]]
   echo "Please install or build QT there and name the subfolder like this: qt-\${QT_VERSION}_vs\${VS_INTERNALVERSION}_(32|64)bit."
   echo "Then set the QT_VERSIONS_PATH on a slave level by configuring the slave in Jenkins."
 else
+  echo "Searching for matching subfolder in $QT_VERSIONS_PATH"
   export QT_QMAKE_BIN_PATH=$(find $(cygpath -m $QT_VERSIONS_PATH) -type d -name "qt-4*_vs${VS_NR}_${ARCH_NO_BIT}bit" | head -1)/bin
   echo "Found matching QT bin dir at: $QT_QMAKE_BIN_PATH"
 fi
