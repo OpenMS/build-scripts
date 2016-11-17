8ff0d321240f1daaa6fe2c747dc812f51c6f7946
Julianus Pfeuffer <jpfeuffer@fu-berlin.de>
2016-11-18 00:10:22 +0100

    weird mac stuff. forget about automation there.

--- platformScripts/shell/macOS/installCommandlineTools.sh
+++ platformScripts/shell/macOS/installCommandlineTools.sh
@@ -8,18 +8,23 @@ then
   then
     "Could not check if license is accepted. Are you sudo? Skipping check."
   fi
+  export SYSROOT=$(xcrun --show-sdk-path | tail -1)
+  if [[ -z $(ls /usr/include) ]]
+  then
+    echo "xcodebuild found, but no /usr/include headers which are e.g. needed for contrib. Please install the Xcode Command Line Developer Tools with sudo xcode-select --install."
+  fi
 else
-  echo "xcodebuild not found. Trying to install.. (mechanism only works on 10.9+)"
-  sudo xcode-select --install
-  sleep 1
-  osascript <<EOD
-    tell application "System Events"
-      tell process "Install Command Line Developer Tools"
-        keystroke return
-        click button "Agree" of window "License Agreement"
-      end tell
-    end tell
-EOD
-  sudo xcodebuild -license accept || echo "Cannot accept XCode license. XCode install not successful? Do you have sudo permissions?"
+  echo "xcodebuild not found. Trying to install.. (mechanism only works on 10.9+). Probably it doesnt. Please just install XCode (AppStore) and the Command Line Developer tools (sudo xcode-select --install)."
+  #sudo xcode-select --install
+  #sleep 1
+  #osascript <<EOD
+  #  tell application "System Events"
+  #    tell process "Install Command Line Developer Tools"
+  #      keystroke return
+  #      click button "Agree" of window "License Agreement"
+  #    end tell
+  #  end tell
+#EOD
+#  sudo xcodebuild -license accept || echo "Cannot accept XCode license. XCode install not successful? Do you have sudo permissions?"
 fi
 
