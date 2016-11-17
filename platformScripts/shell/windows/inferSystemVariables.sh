9e7c91e5ec0e9b47a6e3e95c2b2a2c8b371b8c78
Julianus Pfeuffer <jpfeuffer@fu-berlin.de>
2016-11-17 20:50:19 +0100

    Change in vs wrapper

--- platformScripts/shell/windows/inferSystemVariables.sh
+++ platformScripts/shell/windows/inferSystemVariables.sh
@@ -16,7 +16,9 @@ export CONTRIB_URL="https://abibuilder.informatik.uni-tuebingen.de/archive/openm
 
 ## We need that wrapper for CMake to load all the correct environment variables. We could also replicate what the bat file does.
 function runNative {
-    eval vssetup="\$VS${VS_NR}0COMNTOOLS\\vsvarsall.bat ${TARGET_ARCH}"
+    
+    eval vssetup="\$VS${VS_NR}0COMNTOOLS\\vsvarsall.bat"
+    vssetup="$vssetup ${TARGET_ARCH}"
     cmd /Q /C call "$vssetup" "&&" "${@}"
 }
 
