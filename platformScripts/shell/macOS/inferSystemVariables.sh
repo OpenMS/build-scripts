1ff4b09e8386e32cedc6c3e94187080a29afec84
Julianus Pfeuffer <jpfeuffer@fu-berlin.de>
2016-11-17 18:44:08 +0100

    We do not need build type for contrib. Always release and debug built.

--- platformScripts/shell/macOS/inferSystemVariables.sh
+++ platformScripts/shell/macOS/inferSystemVariables.sh
@@ -1,7 +1,7 @@
 sourceHere ./updatePackageManager.sh
 export SUBDISTRO_NAME="macOS"
 export SUBDISTRO_VERSION=$(sw_vers -productVersion)
-export CONTRIB_URL="https://abibuilder.informatik.uni-tuebingen.de/archive/openms/contrib/$OPSYS/$SUBDISTRO_VERSION/$TARGET_ARCH/$COMPILER/$BUILD_TYPE/contrib_build.tar.gz"
+export CONTRIB_URL="https://abibuilder.informatik.uni-tuebingen.de/archive/openms/contrib/$OPSYS/$SUBDISTRO_VERSION/$TARGET_ARCH/$COMPILER/contrib_build.tar.gz"
 ## Just pass and execute the arguments
 export function runNative {
   eval $(printf "%q " "$@")
