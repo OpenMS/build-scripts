779268a3624f9fd0be69fdf437550d643c7c0588
Julianus Pfeuffer <jpfeuffer@fu-berlin.de>
2016-11-18 23:13:42 +0100

    gcc on mac

--- platformScripts/shell/macOS/installCompiler.sh
+++ platformScripts/shell/macOS/installCompiler.sh
@@ -10,14 +10,16 @@ if [[ $1 =~ ^appleclang.*$ ]]
   echo "Installed $COMPILER_ID"
 elif [[ $1 =~ ^g\+\+.*$ ]]
     then
-    formulaname=${1/+/c}
-    brew tap homebrew/versions >> $LOG_PATH/packages.log 2>&1
-    brew install $formulaname >> $LOG_PATH/packages.log 2>&1
+    ## TODO formula name is actually something like gcc48
+    ## assume installed for now
+    #formulaname=${1/+/c}
+    #brew tap homebrew/versions >> $LOG_PATH/packages.log 2>&1
+    #brew install $formulaname >> $LOG_PATH/packages.log 2>&1
     if [[ -z $($1 -dumpversion) ]]
     then
       export COMPILER_ID="g++-$($1 -dumpversion)"
       export CXX=$(which $1)
-      export CC=$(which $formulaname)
+      export CC=$(which ${1/+/c})
       export GENERATOR="Unix Makefiles"
       ## TODO probably not gonna work. I think you NEED the command line tools then
       export ADDITIONAL_CMAKE_ARGUMENTS="-DCMAKE_OSX_SYSROOT=$SYSROOT"
