f7294daa06a8436ec3a6fb0576497ea919322a7a
Julianus Pfeuffer <jpfeuffer@fu-berlin.de>
2016-11-18 13:52:56 +0100

    Not correctly quoted

--- platformScripts/shell/windows/inferSystemVariables.sh
+++ platformScripts/shell/windows/inferSystemVariables.sh
@@ -17,7 +17,7 @@ export CONTRIB_URL="https://abibuilder.informatik.uni-tuebingen.de/archive/openm
 ## We need that wrapper for CMake to load all the correct environment variables. We could also replicate what the bat file does.
 function runNative {
     eval vcpath="\$VS${VS_NR}0COMNTOOLS..\\..\\VC"
-    vcpathcyg=$(cygpath -m $vcpath)
+    vcpathcyg="$(cygpath -m $vcpath)"
     vssetup="$vcpathcyg/vsvarsall.bat"
     cmd /Q /C call "$vssetup" "${TARGET_ARCH}" "&&" "${@}"
 }
