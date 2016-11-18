37948586fbc455c5fd2171eb8f1cb26884939148
Julianus Pfeuffer <jpfeuffer@fu-berlin.de>
2016-11-18 23:19:20 +0100

    NOT

--- platformScripts/shell/macOS/installCompiler.sh
+++ platformScripts/shell/macOS/installCompiler.sh
@@ -15,9 +15,9 @@ elif [[ $1 =~ ^g\+\+.*$ ]]
     #formulaname=${1/+/c}
     #brew tap homebrew/versions >> $LOG_PATH/packages.log 2>&1
     #brew install $formulaname >> $LOG_PATH/packages.log 2>&1
-    if [[ -z $($1 --version) ]]
+    if ! [[ -z $($1 -dumpversion) ]]
     then
-      export COMPILER_ID="g++-$($1 --version)"
+      export COMPILER_ID="g++-$($1 -dumpversion)"
       export CXX=$(which $1)
       export CC=$(which ${1/+/c})
       export GENERATOR="Unix Makefiles"
@@ -25,7 +25,7 @@ elif [[ $1 =~ ^g\+\+.*$ ]]
       export ADDITIONAL_CMAKE_ARGUMENTS="-DCMAKE_OSX_SYSROOT=$SYSROOT"
       echo "Installed $COMPILER_ID"
     else
-      echo "Compiler installation failed. Check package name, repo settings/availability and the script $0."    
+      echo "Compiler installation of $1 failed. Check package name, repo settings/availability and the script $0."    
     fi
 elif [[ $1 =~ ^clang.*$ ]]
     then
