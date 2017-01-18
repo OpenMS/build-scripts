aac3b9e1f9d08b79debcd1b1a0f0c4f0d13723eb
jpfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2017-01-18 18:54:52 +0100

    Changed message

--- platformScripts/shell/linux/startWindowServer.sh
+++ platformScripts/shell/linux/startWindowServer.sh
@@ -1,8 +1,7 @@
 if [[ -z $DISPLAY ]]
   then
-    tick "Setting up virtual X-Server"
+    tick "Installing and starting virtual X-Server"
     source setupXvfb.sh
-    tock
   else
-  echo "Window System running on Display $DISPLAY. Using this.."
+    echo "Window System running on Display $DISPLAY. Using this.."
 fi
