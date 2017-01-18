5bd661a76fa7fa70bc1e86fd7f487db6a0a71fdd
jpfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2017-01-18 18:38:15 +0100

    Remove subfolder

--- platformScripts/shell/linux/startWindowServer.sh
+++ platformScripts/shell/linux/startWindowServer.sh
@@ -1,7 +1,7 @@
 if [[ -z $DISPLAY ]]
   then
     tick "Setting up virtual X-Server"
-    sourceHere $OPSYS/setupXvfb.sh
+    source setupXvfb.sh
     tock
   else
   echo "Window System running on Display $DISPLAY. Using this.."
