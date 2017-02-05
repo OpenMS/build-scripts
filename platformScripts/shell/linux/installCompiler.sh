85fcebf8d6aed1627a9555d58e98503deb7b9254
jpfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2017-02-05 14:52:51 +0100

    pthread flag

--- platformScripts/shell/linux/installCompiler.sh
+++ platformScripts/shell/linux/installCompiler.sh
@@ -1,2 +1,6 @@
 sourceHere $DISTRO/installCompiler.sh $1
 export GENERATOR="Unix Makefiles"
+## Somehow needed for Clang because of this dependency Superhirn->Xerces
+## Maybe can be added in OpenMS CMake at some point
+export CFLAGS="-pthread"
+export CXXFLAGS="-pthread"
