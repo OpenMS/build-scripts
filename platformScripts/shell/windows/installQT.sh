e3ad976fd5f5a7768b35aa38e243e1728df1a778
jpfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2017-05-24 17:11:53 +0200

    Update installQT.sh

--- platformScripts/shell/windows/installQT.sh
+++ platformScripts/shell/windows/installQT.sh
@@ -2,7 +2,8 @@ if [[ -z $QT_VERSIONS_PATH ]]
   then
   echo "WARNING: On Windows slaves, we currently require a prebuilt QT in a subfolder of the folder set in the QT_VERSIONS_PATH variable."
   echo "Please install or build QT there and name the subfolder like this: qt-\${QT_VERSION}-vs\${VS_YEAR}-(32|64)bit."
-  echo "Then set the QT_VERSIONS_PATH on a slave level by configuring the slave in Jenkins."
+  echo "Then set the QT_VERSIONS_PATH on a slave level by configuring the slave in Jenkins. Please use mixed cygpath representation,"
+  echo " e.g., C:/dev/qt-builds"
 else
   echo "Searching for matching subfolder in $QT_VERSIONS_PATH"
   export QT_QMAKE_BIN_PATH=$(/usr/bin/find $(cygpath -m $QT_VERSIONS_PATH) -maxdepth 1 -type d -name "qt-4.8*-vs${VS_YEAR}-${ARCH_NO_BIT}bit" | head -1)/bin
