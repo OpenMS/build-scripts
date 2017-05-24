8437dc6b268fe3bb392e112c5868535e04df9347
jpfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2017-05-24 17:36:14 +0200

    Update prepareOpenMSBuild.sh

--- platformScripts/shell/prepareOpenMSBuild.sh
+++ platformScripts/shell/prepareOpenMSBuild.sh
@@ -34,10 +34,10 @@ if $DOWNLOAD_CONTRIB
   if ! $USE_DISTRO_CONTRIB
   then
     echo "Downloading full contrib build from archive $CONTRIB_URL..."
-    curl --remote-name $CONTRIB_URL > $LOG_PATH/contrib_setup.log 2>&1
+    curl -O $CONTRIB_URL > $LOG_PATH/contrib_setup.log 2>&1
     # Archive should contain no root folder, only bin, lib, include etc. (see the archiving step of the openms_contrib_all job)
     # force-local to allow usage of mixed POSIX/Win paths (e.g. starting with C:), otherwise interpreted as remote location
-    tar --force-local -xvzf $WORKSPACE/contrib_build.tar.gz --directory $CONTRIB_PATH >> $LOG_PATH/contrib_setup.log 2>&1
+    tar --force-local -xvzf contrib_build.tar.gz --directory $CONTRIB_PATH >> $LOG_PATH/contrib_setup.log 2>&1
   else
     # Install as much as possible from the package managers
     # Build or download prebuild for the rest
