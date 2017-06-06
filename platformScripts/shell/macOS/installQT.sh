dc28e2979cd28fc7e00f01f3264769bb412456fd
jpfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2017-06-06 20:53:52 +0200

    Introduced OPENMS_BREW_FOLDER variable for Mac
    This can be a private brew or a system-wide brew.

--- platformScripts/shell/macOS/installQT.sh
+++ platformScripts/shell/macOS/installQT.sh
@@ -1,2 +1,4 @@
-#TODO brew
-export QT_QMAKE_BIN_PATH=/usr/local/bin
+#${OPENMS_BREW_FOLDER}/bin/brew tap cartr/qt4
+#${OPENMS_BREW_FOLDER}/bin/brew install qt@4
+#${OPENMS_BREW_FOLDER}/bin/brew install qt-webkit@2.3
+export QT_QMAKE_BIN_PATH=${OPENMS_BREW_FOLDER}/bin
