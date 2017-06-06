9e6dd15c79465b30eee656df06a5046e65fe95a9
jpfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2017-06-06 21:58:39 +0200

    Update installPythonAndPip.sh

--- platformScripts/shell/macOS/installPythonAndPip.sh
+++ platformScripts/shell/macOS/installPythonAndPip.sh
@@ -1,6 +1,10 @@
 #python2.7 is std on Mac
+echo "Using standard Python 2.7 preinstalled on Mac"
 if ! [ -x "$(command -v pip)" ]
   then
+  echo "Installing pip..."
   curl -O https://bootstrap.pypa.io/get-pip.py >> $LOG_PATH/packages.log
   python get-pip.py --user >> $LOG_PATH/packages.log
+else
+  echo "pip already installed. Skip."
 fi
