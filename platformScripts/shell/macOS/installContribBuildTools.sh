4839fb1325e7fd09272f995e5d78a4f1b9b9949e
jpfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2017-06-06 18:16:15 +0200

    Update installContribBuildTools.sh

--- platformScripts/shell/macOS/installContribBuildTools.sh
+++ platformScripts/shell/macOS/installContribBuildTools.sh
@@ -1,2 +1,3 @@
+sourceHere installCommandlineTools.sh
 ## tar and patch are standard on macOS, libtool via brew installs glibtoolize with a g prepended (CMake will take care of this).
 brew install cmake autoconf automake libtool >> $LOG_PATH/packages.log 2>&1
