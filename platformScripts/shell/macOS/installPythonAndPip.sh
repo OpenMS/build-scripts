177bc6eae04559d0078abd341b187267e961ef18
jpfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2017-06-06 21:57:31 +0200

    Added logging

--- platformScripts/shell/macOS/installPythonAndPip.sh
+++ platformScripts/shell/macOS/installPythonAndPip.sh
@@ -1,6 +1,6 @@
 #python2.7 is std on Mac
 if ! [ -x "$(command -v pip)" ]
   then
-  curl -O https://bootstrap.pypa.io/get-pip.py
-  python get-pip.py --user
+  curl -O https://bootstrap.pypa.io/get-pip.py >> $LOG_PATH/packages.log
+  python get-pip.py --user >> $LOG_PATH/packages.log
 fi
