bc68fd71f71901dc17ff2c34cbd5d0a6b1d77532
jpfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2017-06-09 16:27:53 +0200

    Added OPENMS_BREW_FOLDER to path on macOS

--- platformScripts/shell/macOS/inferSystemVariables.sh
+++ platformScripts/shell/macOS/inferSystemVariables.sh
@@ -4,6 +4,8 @@ export SUBDISTRO_VERSION=$(sw_vers -productVersion)
 export OPENMS_TARGET_ARCH=${ARCH}
 export REMOTE_CONTRIB_FOLDER="contrib/$OPSYS/$SUBDISTRO_VERSION/$OPENMS_TARGET_ARCH/$COMPILER/"
 export CONTRIB_URL="https://abibuilder.informatik.uni-tuebingen.de/archive/openms/$REMOTE_CONTRIB_FOLDER/contrib_build.tar.gz"
+## Special for macOS to allow multiple brew installations
+export PATH=${PATH};${OPENMS_BREW_FOLDER}
 ## Just pass and execute the arguments
 function runNative {
   echo "Running $@"
