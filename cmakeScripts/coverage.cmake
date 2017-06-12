a07f1548ff633ec97e48b39db8bb452d7df72b39
jpfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2017-06-13 01:04:44 +0200

    Update coverage.cmake

--- cmakeScripts/coverage.cmake
+++ cmakeScripts/coverage.cmake
@@ -40,7 +40,7 @@ set(required_variables "CTEST_SOURCE_DIRECTORY;CTEST_BINARY_DIRECTORY;CTEST_BUIL
 backup_and_check_variables(required_variables)
 
 ## OpenMS_Coverage target looks for this file. Since we backup stuff, it might be moved. Touch to create.
-execute_process(${CMAKE_COMMAND} -E touch ${CTEST_BINARY_DIRECTORY}/Testing/Temporary/LastTest.log)
+execute_process(COMMAND ${CMAKE_COMMAND} -E touch ${CTEST_BINARY_DIRECTORY}/Testing/Temporary/LastTest.log)
 
 if(NOT DEFINED CDASH_SUBMIT)
     set(CDASH_SUBMIT Off)
