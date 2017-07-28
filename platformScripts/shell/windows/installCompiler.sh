8886737d99cf8f08d56f013c0f162ad38638efed
jpfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2017-07-28 16:02:38 +0200

    Fix support for VS2017

--- platformScripts/shell/windows/installCompiler.sh
+++ platformScripts/shell/windows/installCompiler.sh
@@ -1,6 +1,7 @@
 if [[ $1 =~ ^msvc-.*$ ]]
 then
   export VS_NR=${1/msvc-/}
+  ## Works starting from VS2010
   if (( VS_NR < "11" ))
     then
     offset=0
@@ -13,7 +14,8 @@ then
   ((VS_YEAR=VS_NR+2000+offset))
   export VS_YEAR
   export COMPILER_ID=$1
-  export GENERATOR="Visual Studio ${VS_NR}${GENERATOR_ARCH_SUFFIX}"
+  
+  export GENERATOR="Visual Studio ${VS_NR} ${VS_YEAR}${GENERATOR_ARCH_SUFFIX}"
   echo "Using GENERATOR=$GENERATOR"
 else
   echo "Unsupported compiler $1 on windows."
