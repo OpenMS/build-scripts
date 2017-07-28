adad615302ec6190f967d3afeaae44a91ca9b3d5
jpfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2017-07-28 18:07:01 +0200

    Add virtualenv to standard python requirements on centos

--- platformScripts/shell/linux/redhat/installPythonAndPip.sh
+++ platformScripts/shell/linux/redhat/installPythonAndPip.sh
@@ -1,3 +1,3 @@
 sudo yum -y install epel-release >> $LOG_PATH/packages.log 2>&1 
 sudo yum -y update >> $LOG_PATH/packages.log 2>&1
-sudo yum -y install python-devel python-pip >> $LOG_PATH/packages.log 2>&1
+sudo yum -y install python-devel python-pip python-virtualenv >> $LOG_PATH/packages.log 2>&1
