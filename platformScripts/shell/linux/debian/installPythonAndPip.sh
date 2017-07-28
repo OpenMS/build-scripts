1601de0152dc154216d3896cbafb3d20955b1e62
jpfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2017-07-28 18:02:56 +0200

    add virtualenv to standard python install

--- platformScripts/shell/linux/debian/installPythonAndPip.sh
+++ platformScripts/shell/linux/debian/installPythonAndPip.sh
@@ -1,1 +1,1 @@
-sudo apt-get -y install python python-dev python-pip >> $LOG_PATH/packages.log 2>&1
+sudo apt-get -y install python python-dev python-pip python-virtualenv >> $LOG_PATH/packages.log 2>&1
