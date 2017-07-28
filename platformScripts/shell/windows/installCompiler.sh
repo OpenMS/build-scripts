9c0b1c875bfeb6eceba2226b98539c1d83013665
jpfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2017-07-28 15:35:57 +0200

    Support VS2017

--- platformScripts/shell/windows/installCompiler.sh
+++ platformScripts/shell/windows/installCompiler.sh
@@ -1,12 +1,16 @@
 if [[ $1 =~ ^msvc-.*$ ]]
 then
-    export VS_NR=${1/msvc-/}
-    if (( VS_NR < "11" ))
+  export VS_NR=${1/msvc-/}
+  if (( VS_NR < "11" ))
     then
-      ((VS_YEAR=VS_NR+2000))
-    else
-      ((VS_YEAR=VS_NR+2001))
-    fi
+    offset=0
+  elif (( VS_NR < "15" ))
+    then
+    offset=1
+  else
+    offset=2
+  fi
+  ((VS_YEAR=VS_NR+2000+offset))
   export VS_YEAR
   export COMPILER_ID=$1
   export GENERATOR="Visual Studio ${VS_NR}${GENERATOR_ARCH_SUFFIX}"
