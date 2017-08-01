d0383bbd1e827b52538d852eefddd807a3846abf
jpfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2017-08-01 22:32:44 +0200

    Update inferSystemVariables.sh

--- platformScripts/shell/windows/inferSystemVariables.sh
+++ platformScripts/shell/windows/inferSystemVariables.sh
@@ -30,13 +30,13 @@ function runNative {
       then
       eval vcpath="\$VS${VS_NR}0COMNTOOLS..\\\\..\\\\VC"
       vcpathcyg=$(cygpath -m $vcpath)
-      vssetup="$vcpathcyg/vcvarsall.bat"
-      varsetupcommand="$vssetup "${OPENMS_TARGET_ARCH}""
+      vssetup="\"$vcpathcyg/vcvarsall.bat\""
+      varsetupcommand="$vssetup ${OPENMS_TARGET_ARCH}"
     else
       vcpath=$("$PROGRAMFILES/Microsoft Visual Studio/Installer/vswhere" -all -property installationPath)
       vcpathcyg=$(cygpath -m $vcpath)/Common7/Tools
-      vssetup="$vcpathcyg/VsDevCmd.bat"
-      varsetupcommand="$vssetup"
+      vssetup="\"$vcpathcyg/VsDevCmd.bat\""
+      varsetupcommand=$vssetup
     fi
     
     ## In MinGW/Git for Windows the slashes need to be escaped additionally
@@ -50,6 +50,6 @@ function runNative {
     ## Be careful. Might suffer from "Input line too long" problem if the CMake command is too long.
     ## Did not happen yet.
     echo Calling: cmd ${slashes}Q ${slashes}C \"call\" "$varsetupcommand" "&&" "${@}" 
-    cmd ${slashes}Q ${slashes}C "call" "$varsetupcommand" "&&" ${@}
+    cmd ${slashes}Q ${slashes}C "call" $varsetupcommand "&&" ${@}
 }
 
