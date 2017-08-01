759b7e1798b9097f250547cdeec0762c0b8febfd
jpfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2017-08-01 21:00:40 +0200

    Update inferSystemVariables.sh

--- platformScripts/shell/windows/inferSystemVariables.sh
+++ platformScripts/shell/windows/inferSystemVariables.sh
@@ -31,11 +31,11 @@ function runNative {
       eval vcpath="\$VS${VS_NR}0COMNTOOLS..\\\\..\\\\VC"
       vcpathcyg=$(cygpath -m "$vcpath")
       vssetup='"""'$vcpathcyg'/vcvarsall.bat"""'
-      varsetupcommand="$vssetup ${OPENMS_TARGET_ARCH}"
+      varsetupcommand="$vssetup \"${OPENMS_TARGET_ARCH}\""
     else
       vcpath=$("$PROGRAMFILES/Microsoft Visual Studio/Installer/vswhere" -all -property installationPath)
       vcpathcyg=$(cygpath -m "$vcpath")/Common7/Tools
-      vssetup='"""'$vcpathcyg'/VsDevCmd.bat"""'
+      vssetup='"'$vcpathcyg'/VsDevCmd.bat"'
       varsetupcommand="$vssetup"
     fi
     
@@ -49,7 +49,7 @@ function runNative {
     fi
     ## Be careful. Might suffer from "Input line too long" problem if the CMake command is too long.
     ## Did not happen yet.
-    echo Calling: cmd ${slashes}Q ${slashes}C call \"\"$varsetupcommand && ${@}\"\" 
-    cmd ${slashes}Q ${slashes}C call \"\"$varsetupcommand && ${@}\"\"
+    echo Calling: cmd ${slashes}Q ${slashes}C "call" $varsetupcommand "&&" "${@}" 
+    cmd ${slashes}Q ${slashes}C "call" $varsetupcommand "&&" "${@}"
 }
 
