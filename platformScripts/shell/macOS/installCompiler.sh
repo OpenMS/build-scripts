db500be486f7e7f06ec13b94bd5fc3b9699120aa
jpfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2017-08-02 19:34:53 +0200

    Update installCompiler.sh

--- platformScripts/shell/macOS/installCompiler.sh
+++ platformScripts/shell/macOS/installCompiler.sh
@@ -9,11 +9,11 @@ if [[ $1 =~ ^appleclang.*$ ]]
   ## If you have multiple compilers installed you should set a correct
   ## sysroot to find the right stdlibs for example
   ##
-  echo ${SYSROOT+x}
-  if [[ -z ${SYSROOT+x} ]]
+  echo ${MYSYSROOT+x}
+  if [[ -z ${MYSYSROOT+x} ]]
     then
-    echo ${SYSROOT+x}
-    export ADDITIONAL_CMAKE_ARGUMENTS="${ADDITIONAL_CMAKE_ARGUMENTS-} -DCMAKE_OSX_SYSROOT=${SYSROOT-}"
+    echo ${MYSYSROOT+x}
+    export ADDITIONAL_CMAKE_ARGUMENTS="${ADDITIONAL_CMAKE_ARGUMENTS-} -DCMAKE_OSX_SYSROOT=${MYSYSROOT-}"
     echo ${ADDITIONAL_CMAKE_ARGUMENTS}
   else
     export ADDITIONAL_CMAKE_ARGUMENTS="${ADDITIONAL_CMAKE_ARGUMENTS-}"
