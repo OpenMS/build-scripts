f89861317a6922cdfc34d1c89bee918c8dd099e1
jpfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2017-08-10 17:09:33 +0200

    Loosen qt version check for now

--- platformScripts/shell/windows/installQT.sh
+++ platformScripts/shell/windows/installQT.sh
@@ -8,10 +8,17 @@ else
   echo "Searching for matching subfolder in $QT_VERSIONS_PATH"
   if [[ ${GIT_BRANCH} =~ .*qt5.* ]]
   then
-    QTVER=5.9
+    ## We can be more specific in the future
+    QTVER=5
   else
-    QTVER=4.8
+    QTVER=4
   fi
-  export QT_QMAKE_BIN_PATH=$(/usr/bin/find $(cygpath -m $QT_VERSIONS_PATH) -maxdepth 1 -type d -name "qt-${QTVER}*-vs${VS_YEAR}-${ARCH_NO_BIT}bit" | head -1)/bin
-  echo "Found matching QT bin dir at: $QT_QMAKE_BIN_PATH"
+  tmp_qt_path=$(/usr/bin/find $(cygpath -m $QT_VERSIONS_PATH) -maxdepth 1 -type d -name "qt-${QTVER}*-vs${VS_YEAR}-${ARCH_NO_BIT}bit" | head -1)
+  if [ ! -z "$tmp_qt_path" ]
+  then
+    export QT_QMAKE_BIN_PATH="${tmp_qt_path}/bin"
+    echo "Found matching QT bin dir at: $QT_QMAKE_BIN_PATH"
+  else
+    echo "No matching QT directory found. Exiting! Set QT_VERSIONS_PATH in the node settings in Jenkins and have subfolders of the form: qt-\${QTVER}*-vs\${VS_YEAR}-\${ARCH_NO_BIT}bit" && exit 1
+  fi
 fi
