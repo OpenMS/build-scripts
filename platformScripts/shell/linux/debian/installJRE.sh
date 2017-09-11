4fff8b0aec3708bdd267e1626645e01ad5afe566
jpfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2017-09-11 16:30:05 +0200

    Update installJRE.sh

--- platformScripts/shell/linux/debian/installJRE.sh
+++ platformScripts/shell/linux/debian/installJRE.sh
@@ -1,1 +1,2 @@
-sudo apt-get -y install default-jre-headless >> $LOG_PATH/packages.log 2>&1
+## Assumes a package repo with openjdk-8 is loaded (not standard on 14.04)
+sudo apt-get -y install openjdk-8-jre-headless >> $LOG_PATH/packages.log 2>&1
