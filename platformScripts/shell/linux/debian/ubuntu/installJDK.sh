1d4bf7cf3df17d16453aa5c71b16354a470575df
jpfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2017-09-11 20:06:27 +0200

    Update installJDK.sh

--- platformScripts/shell/linux/debian/ubuntu/installJDK.sh
+++ platformScripts/shell/linux/debian/ubuntu/installJDK.sh
@@ -1,2 +1,3 @@
+## We do not need a branch here anymore. Java8 is minimum anyway. Make sure Ubuntu14.04 has a package repo loaded that includes this package.
 #sourceHere $SUBDISTRO_VERSION/installJDK.sh
-sudo apt-get -y install default-jdk >> $LOG_PATH/packages.log 2>&1
+sudo apt-get -y install openjdk-8-jdk >> $LOG_PATH/packages.log 2>&1
