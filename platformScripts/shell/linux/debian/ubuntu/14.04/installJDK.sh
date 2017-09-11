4a9ab5fbd51ba1773ba6307311574e52c6564b0b
jpfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2017-09-11 16:28:20 +0200

    Update installJDK.sh

--- platformScripts/shell/linux/debian/ubuntu/14.04/installJDK.sh
+++ platformScripts/shell/linux/debian/ubuntu/14.04/installJDK.sh
@@ -1,2 +1,2 @@
-## MSGF+ requires java 8
+## MSGF+ requires java 8. Assumes the there is a package repo loaded that includes openjdk-8
 sudo apt-get -y install openjdk-8-jdk >> $LOG_PATH/packages.log 2>&1
