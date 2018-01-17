7044d0a07855e70c3e57a78fbe2189e03ba49bd1
jpfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2018-01-17 18:28:22 +0100

    Add default brew installation as fallback

--- platformScripts/shell/macOS/inferSystemVariables.sh
+++ platformScripts/shell/macOS/inferSystemVariables.sh
@@ -4,6 +4,11 @@ export OPENMS_TARGET_ARCH=${ARCH}
 export REMOTE_CONTRIB_FOLDER="contrib/$OPSYS/$SUBDISTRO_VERSION/$OPENMS_TARGET_ARCH/$COMPILER/"
 export CONTRIB_URL="https://abibuilder.informatik.uni-tuebingen.de/archive/openms/$REMOTE_CONTRIB_FOLDER/contrib_build.tar.gz"
 ## Special for macOS to allow multiple brew installations
+if [ -z "${OPENMS_BREW_FOLDER+x}" ]
+then
+  echo "OPENMS_BREW_FOLDER was not set. Using standard brew installation under /usr/local/Homebrew"
+  export OPENMS_BREW_FOLDER="/usr/local/Homebrew"
+fi
 export PATH="${PATH}:${OPENMS_BREW_FOLDER}/bin"
 export OPENMS_BREW="${OPENMS_BREW_FOLDER}/bin/brew"
 ## Just pass and execute the arguments
