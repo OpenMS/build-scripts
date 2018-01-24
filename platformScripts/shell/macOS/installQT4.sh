f51c4199235bbe4974442d24fa1edbd5ab7c33aa
jpfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2018-01-24 22:15:19 +0100

    Split into QT and QT_WEBKIT roots

--- platformScripts/shell/macOS/installQT4.sh
+++ platformScripts/shell/macOS/installQT4.sh
@@ -1,4 +1,5 @@
 #${OPENMS_BREW_FOLDER}/bin/brew tap cartr/qt4
 #${OPENMS_BREW_FOLDER}/bin/brew install qt@4
 #${OPENMS_BREW_FOLDER}/bin/brew install qt-webkit@2.3
-export QT_ROOT="$(${OPENMS_BREW_FOLDER}/bin/brew --prefix qt@4);$(${OPENMS_BREW_FOLDER}/bin/brew --prefix qt-webkit@2.3)"
+export QT_ROOT="$(${OPENMS_BREW_FOLDER}/bin/brew --prefix qt@4)"
+export QT_WEBKIT_ROOT="$(${OPENMS_BREW_FOLDER}/bin/brew --prefix qt-webkit@2.3)"
