1e4da826e2db0f448adf3d337ce24a1f01b5dc41
jpfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2018-01-24 17:54:04 +0100

    Update installJDK.sh

--- platformScripts/shell/linux/debian/ubuntu/installJDK.sh
+++ platformScripts/shell/linux/debian/ubuntu/installJDK.sh
@@ -1,3 +1,4 @@
 ## We do not need a branch here anymore. Java8 is minimum anyway. Make sure Ubuntu14.04 has a package repo loaded that includes this package.
-#sourceHere $SUBDISTRO_VERSION/installJDK.sh
+## Ubuntu 14.04 (already on docker image): sudo add-apt-repository ppa:openjdk-r/ppa
+## Ubuntu 14.04 (already on docker image): sudo apt-get update
 sudo apt-get -y install openjdk-8-jdk >> $LOG_PATH/packages.log 2>&1
