617e9f0bc5bdfdca45c0d66470eca64a56497178
jpfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2018-01-24 21:26:58 +0100

    Update installQT5.sh

--- platformScripts/shell/macOS/installQT5.sh
+++ platformScripts/shell/macOS/installQT5.sh
@@ -1,2 +1,2 @@
 #${OPENMS_BREW_FOLDER}/bin/brew install qt5
-export QT_QMAKE_BIN_PATH=$(${OPENMS_BREW_FOLDER}/bin/brew --prefix qt5)/bin
+export QT_ROOT=$(${OPENMS_BREW_FOLDER}/bin/brew --prefix qt5)
