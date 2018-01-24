eb7542c2af2bdaac2e005ce74f9e672bf408b285
jpfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2018-01-24 21:26:42 +0100

    Update installQT4.sh

--- platformScripts/shell/macOS/installQT4.sh
+++ platformScripts/shell/macOS/installQT4.sh
@@ -1,4 +1,4 @@
 #${OPENMS_BREW_FOLDER}/bin/brew tap cartr/qt4
 #${OPENMS_BREW_FOLDER}/bin/brew install qt@4
 #${OPENMS_BREW_FOLDER}/bin/brew install qt-webkit@2.3
-export QT_QMAKE_BIN_PATH=${OPENMS_BREW_FOLDER}/bin
+export QT_ROOT=${OPENMS_BREW_FOLDER}
