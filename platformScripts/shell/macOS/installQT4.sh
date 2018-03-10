20e0b30fa2da4186b0b589f6936883f1fc21022b
jpfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2018-03-10 13:14:26 +0100

    Update installQT4.sh

--- platformScripts/shell/macOS/installQT4.sh
+++ platformScripts/shell/macOS/installQT4.sh
@@ -1,5 +1,7 @@
 #${OPENMS_BREW_FOLDER}/bin/brew tap cartr/qt4
 #${OPENMS_BREW_FOLDER}/bin/brew install qt@4
 #${OPENMS_BREW_FOLDER}/bin/brew install qt-webkit@2.3
-export QT_ROOT="$(${OPENMS_BREW_FOLDER}/bin/brew --prefix qt@4)"
-export QT_WEBKIT_ROOT="$(${OPENMS_BREW_FOLDER}/bin/brew --prefix qt-webkit@2.3)"
+## First line is for older brews where qt was still qt4. Will be overridden if the newer custom packages exist
+${OPENMS_BREW_FOLDER}/bin/brew --prefix qt && export QT_ROOT="$(${OPENMS_BREW_FOLDER}/bin/brew --prefix qt)"
+${OPENMS_BREW_FOLDER}/bin/brew --prefix qt@4 && export QT_ROOT="$(${OPENMS_BREW_FOLDER}/bin/brew --prefix qt@4)"
+${OPENMS_BREW_FOLDER}/bin/brew --prefix qt-webkit@2.3 && export QT_WEBKIT_ROOT="$(${OPENMS_BREW_FOLDER}/bin/brew --prefix qt-webkit@2.3)"
