1f77a5ebc113e44fe42ddb01228a4b70d734b117
jpfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2018-03-23 18:02:55 +0100

    Only set QT_ROOT if not set before

--- platformScripts/shell/linux/installQT5.sh
+++ platformScripts/shell/linux/installQT5.sh
@@ -1,2 +1,6 @@
 sourceHere $DISTRO/installQT5.sh
-export QT_ROOT=/usr
+if [ -z ${QT_ROOT+x} ] ## if QT_ROOT is not set by distro specifics we assume it is in usual /usr
+  then 
+  export QT_ROOT=/usr
+fi
+
