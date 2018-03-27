f73d26950cef44c64b0f82334e89d5440c6c3cb1
jpfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2018-03-27 14:57:44 +0200

    Update installQT5.sh

--- platformScripts/shell/linux/debian/ubuntu/16.04/installQT5.sh
+++ platformScripts/shell/linux/debian/ubuntu/16.04/installQT5.sh
@@ -1,4 +1,4 @@
-sudo apt-get -y install software-properties-common
+sudo apt-get -y install software-properties-common libgl1-mesa-dev
 sudo apt-add-repository -y ppa:beineri/opt-qt591-xenial >> $LOG_PATH/packages.log 2>&1
 sudo apt-get -y update >> $LOG_PATH/packages.log 2>&1
 sudo apt-get -y install qt59base qt59svg >> $LOG_PATH/packages.log 2>&1
