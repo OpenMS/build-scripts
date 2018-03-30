3b9011328ca6fa2de33e74dc88496f0a76f67eb5
jpfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2018-03-30 14:34:39 +0200

    load qt environment

--- platformScripts/shell/linux/debian/ubuntu/14.04/installQT5.sh
+++ platformScripts/shell/linux/debian/ubuntu/14.04/installQT5.sh
@@ -2,4 +2,5 @@ sudo apt-get -y install software-properties-common libgl1-mesa-dev
 sudo apt-add-repository -y ppa:beineri/opt-qt591-trusty >> $LOG_PATH/packages.log 2>&1
 sudo apt-get -y update >> $LOG_PATH/packages.log 2>&1
 sudo apt-get -y install qt59base qt59svg >> $LOG_PATH/packages.log 2>&1
+source /opt/qt59/bin/qt*-env.sh
 export QT_ROOT=/opt/qt59
