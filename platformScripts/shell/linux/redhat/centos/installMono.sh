97f7d7d86fed6342a7b71121a6e6df8ce0d7f905
jpfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2019-05-28 15:32:43 +0200

    Create installMono.sh

---
+++ platformScripts/shell/linux/redhat/centos/installMono.sh
@@ -0,0 +1,2 @@
+rpm --import "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF"
+su -c 'curl https://download.mono-project.com/repo/centos${SUBDISTRO_VERSION}-stable.repo | tee /etc/yum.repos.d/mono-centos${SUBDISTRO_VERSION}-stable.repo'
