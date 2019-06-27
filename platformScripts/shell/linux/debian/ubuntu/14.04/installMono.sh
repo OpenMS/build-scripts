e5e67fb4688923376cbcf75d3e88d699d99875c4
jpfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2019-06-27 18:48:43 +0200

    Update installMono.sh

--- platformScripts/shell/linux/debian/ubuntu/14.04/installMono.sh
+++ platformScripts/shell/linux/debian/ubuntu/14.04/installMono.sh
@@ -1,5 +1,5 @@
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-sudo apt -y install apt-transport-https ca-certificates
-echo "deb https://download.mono-project.com/repo/ubuntu stable-wheezy main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
-sudo apt -y update
-sudo apt -y install mono-complete
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF >> $LOG_PATH/packages.log 2>&1
+sudo apt -y install apt-transport-https ca-certificates >> $LOG_PATH/packages.log 2>&1
+echo "deb https://download.mono-project.com/repo/ubuntu stable-wheezy main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list >> $LOG_PATH/packages.log 2>&1
+sudo apt -y update >> $LOG_PATH/packages.log 2>&1
+sudo apt -y install mono-complete >> $LOG_PATH/packages.log 2>&1
