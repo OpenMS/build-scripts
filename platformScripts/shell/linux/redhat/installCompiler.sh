ae090b2cb596396cc347cac4b8266e433d7e0038
jpfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2019-07-30 13:52:38 +0200

    Update installCompiler.sh

--- platformScripts/shell/linux/redhat/installCompiler.sh
+++ platformScripts/shell/linux/redhat/installCompiler.sh
@@ -1,17 +1,25 @@
 sudo yum -y install epel-release && sudo yum -y update >> $LOG_PATH/packages.log 2>&1
 if [[ $1 =~ ^g++.*$ ]]
     then
-    packageregex=${1/g/gcc-c}
-    ## Careful: Maybe only take first match. Might install multiple versions, if version is not specific enough.
-    sudo yum -y install "$packageregex*" >> $LOG_PATH/packages.log 2>&1
-    if ! [[ -z $(g++ -dumpversion) ]]
+    if [[ $(g++ -dumpversion) =~ ^.*"$1".*$ ]]
     then
-      export COMPILER_ID="g++-$(g++ -dumpversion)"
-      export CXX=$(which g++)
-      export CC=$(which gcc)
-      echo "Installed $COMPILER_ID"
-    else 
-      echo "Installation of $1 failed. Check package name, repo settings/availability and the script $0."
+        echo "$(g++ -dumpversion) already installed. Using this."
+        export COMPILER_ID="g++-$(g++ -dumpversion)"
+        export CXX=$(which g++)
+        export CC=$(which gcc)
+    else
+        packageregex=${1/g/gcc-c}
+        ## Careful: Maybe only take first match. Might install multiple versions, if version is not specific enough.
+        sudo yum -y install "$packageregex*" >> $LOG_PATH/packages.log 2>&1
+        if ! [[ -z $(g++ -dumpversion) ]]
+        then
+          export COMPILER_ID="g++-$(g++ -dumpversion)"
+          export CXX=$(which g++)
+          export CC=$(which gcc)
+          echo "Installed $COMPILER_ID"
+        else 
+          echo "Installation of $1 failed. Check package name, repo settings/availability and the script $0."
+        fi
     fi
 elif [[ $1 =~ ^clang.*$ ]]
     then
