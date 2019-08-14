e35465ffd8642c44749d3e05c76290fae06701fe
jpfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2019-08-14 16:10:59 +0200

    Update inferSystemVariables.sh

--- platformScripts/shell/macOS/inferSystemVariables.sh
+++ platformScripts/shell/macOS/inferSystemVariables.sh
@@ -1,5 +1,6 @@
 export SUBDISTRO_NAME="macOS"
 export SUBDISTRO_VERSION=$(sw_vers -productVersion)
+export MACOSX_DEPLOYMENT_TARGET="10.9"
 export OPENMS_TARGET_ARCH=${ARCH}
 export REMOTE_CONTRIB_FOLDER="contrib/$OPSYS/$SUBDISTRO_VERSION/$OPENMS_TARGET_ARCH/$COMPILER/"
 export CONTRIB_URL="https://abibuilder.informatik.uni-tuebingen.de/archive/openms/$REMOTE_CONTRIB_FOLDER/contrib_build.tar.gz"
