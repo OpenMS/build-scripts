fa5df2e9370064f94baa99aa822b93e6adfc8a1d
jpfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2019-09-19 09:52:39 +0200

    Update installDocuTools.sh

--- platformScripts/shell/linux/installDocuTools.sh
+++ platformScripts/shell/linux/installDocuTools.sh
@@ -1,6 +1,7 @@
 ## Currently we use the binary installer for doxygen to get at least 1.8.16 on all Linuxes
 curl -O http://doxygen.nl/files/doxygen-1.8.16.linux.bin.tar.gz
 sudo tar zxf doxygen-1.8.16.linux.bin.tar.gz -C /opt/
-export PATH="/opt/doxygen-1.8.16/bin:$PATH"
+ls -la /opt/doxygen-1.8.16/
 
 sourceHere $DISTRO/installDocuTools.sh
+export PATH="/opt/doxygen-1.8.16/bin:$PATH"
