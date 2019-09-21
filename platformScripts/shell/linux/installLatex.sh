e91b30904118c7b8e873ee3047223772b86c82ca
jpfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2019-09-21 16:02:19 +0200

    Add calc package for doxygen 1.8.16

--- platformScripts/shell/linux/installLatex.sh
+++ platformScripts/shell/linux/installLatex.sh
@@ -17,7 +17,7 @@ rm $TL.tar.gz && rm -r $TL
 # Minimal nr of additional packages for docu (might be preinstalled depending on the install config of TL)
 # Does not include fonts. Always install fonts-recommended and fonts-extra for now.
 # sudo uses different path. Therefore the which.
-sudo $(which tlmgr) install oberdiek amsmath babel carlisle ec geometry lm marvosym graphics-def \
+sudo $(which tlmgr) install oberdiek amsmath babel calc carlisle ec geometry lm marvosym graphics-def \
   latex latex-bin fancyhdr graphics float colortbl xcolor xtab newtx fontaxes xkeyval etoolbox kastrup \
   tex-gyre tools hyperref listings url parskip tocloft > $LOG_PATH/texpackages.log 2>&1
 
