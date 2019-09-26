0c0cdfef5513682312ea3f8c02cc657ce811b029
jpfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2019-09-26 14:57:28 +0200

    Update installCMake.sh

--- platformScripts/shell/linux/installCMake.sh
+++ platformScripts/shell/linux/installCMake.sh
@@ -1,5 +1,5 @@
 # Install a more recent version of CMake to test some 3.1+ features for e.g. coverage tests
 # 3.7+ also fixes CPack issues of corrupted deb files...
-curl -sSL -O https://cmake.org/files/v3.14/cmake-3.14.2-Linux-x86_64.tar.gz
-sudo tar zxf cmake-3.14.2-Linux-x86_64.tar.gz -C /opt/
-export PATH="/opt/cmake-3.14.2-Linux-x86_64/bin:$PATH"
+curl -sSL -O https://cmake.org/files/v3.15/cmake-3.15.3-Linux-x86_64.tar.gz
+sudo tar zxf cmake-3.15.3-Linux-x86_64.tar.gz -C /opt/
+export PATH="/opt/cmake-3.15.3-Linux-x86_64/bin:$PATH"
