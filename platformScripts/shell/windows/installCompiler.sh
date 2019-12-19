0429fb33d5fa20ab8df5347fa0c335bf6db55ae2
jpfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2019-12-19 12:24:22 +0100

    update generator names for vs

--- platformScripts/shell/windows/installCompiler.sh
+++ platformScripts/shell/windows/installCompiler.sh
@@ -1,15 +1,28 @@
+function version { echo "$@" | awk -F. '{ printf("%d%03d%03d%03d\n", $1,$2,$3,$4); }'; }
 if [[ $1 =~ ^msvc-.*$ ]]
 then
   export VS_NR=${1/msvc-/}
-  ## Works starting from VS2010
-  if (( VS_NR < "11" ))
+  if [ $(version $VS_NR) -lt $(version "14.0") ]
     then
-    offset=0
-  elif (( VS_NR < "15" ))
+    echo "VS too old"
+    exit 1
+  elif [ $(version $VS_NR) -lt $(version "14.1") ]
     then
-    offset=1
-  else
-    offset=2
+    export VS_NR=14
+    VS_YEAR=2015
+  elif [ $(version $VS_NR) -lt $(version "14.2") ]
+    then
+    export VS_NR=15
+    VS_YEAR=2017
+  elif [ $(version $VS_NR) -lt $(version "14.3") ]
+    then
+    export VS_NR=16
+    VS_YEAR=2019
+    # By default it builds for the current arch. Which is fine.
+    # If we need to change it in the future we need to use the
+    # -A option of CMake or better introduce CMake toolchain files
+    # on each machine
+    export GENERATOR_ARCH_SUFFIX=""
   fi
   ((VS_YEAR=VS_NR+2000+offset))
   export VS_YEAR
