6e927770704bcc2210dd63ce6f36a620e717852d
jpfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2020-08-10 17:10:02 +0200

    Update doxygen version and download link

--- platformScripts/shell/linux/installDocuTools.sh
+++ platformScripts/shell/linux/installDocuTools.sh
@@ -1,7 +1,8 @@
 ## Currently we use the binary installer for doxygen to get at least 1.8.16 on all Linuxes
-curl -O http://doxygen.nl/files/doxygen-1.8.16.linux.bin.tar.gz
-sudo tar zxf doxygen-1.8.16.linux.bin.tar.gz -C /opt/
-ls -la /opt/doxygen-1.8.16/
+DOXYGEN_VERSION=1.8.19
+curl -O http://doxygen.nl/files/doxygen-${DOXYGEN_VERSION}.linux.bin.tar.gz
+sudo tar zxf doxygen-${DOXYGEN_VERSION}.linux.bin.tar.gz -C /opt/
+ls -la /opt/doxygen-${DOXYGEN_VERSION}/
 
 sourceHere $DISTRO/installDocuTools.sh
-export PATH="/opt/doxygen-1.8.16/bin:$PATH"
+export PATH="/opt/doxygen-${DOXYGEN_VERSION}/bin:$PATH"
