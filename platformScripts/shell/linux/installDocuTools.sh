22f5dd71b21bfa85a280b78f2f980a91a024da91
jpfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2020-08-11 11:22:05 +0200

    follow redirects when downloading doxygen

--- platformScripts/shell/linux/installDocuTools.sh
+++ platformScripts/shell/linux/installDocuTools.sh
@@ -1,6 +1,6 @@
 ## Currently we use the binary installer for doxygen to get at least 1.8.16 on all Linuxes
 DOXYGEN_VERSION=1.8.19
-curl -O http://doxygen.nl/files/doxygen-${DOXYGEN_VERSION}.linux.bin.tar.gz
+curl -L -O http://doxygen.nl/files/doxygen-${DOXYGEN_VERSION}.linux.bin.tar.gz
 sudo tar zxf doxygen-${DOXYGEN_VERSION}.linux.bin.tar.gz -C /opt/
 ls -la /opt/doxygen-${DOXYGEN_VERSION}/
 
