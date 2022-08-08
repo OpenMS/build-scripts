7493270d1855f8948da35ff79e2a87d2f6932a9a
Timo Sachsenberg <timo.sachsenberg@uni-tuebingen.de>
2022-08-08 16:57:55 +0200

    fix wrong package repository in 20.04

--- platformScripts/shell/linux/debian/ubuntu/20.04/installMono.sh
+++ platformScripts/shell/linux/debian/ubuntu/20.04/installMono.sh
@@ -1,5 +1,5 @@
 sudo apt -y install gnupg ca-certificates >> $LOG_PATH/packages.log 2>&1
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF >> $LOG_PATH/packages.log 2>&1
-echo "deb https://download.mono-project.com/repo/ubuntu stable-bionic main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list >> $LOG_PATH/packages.log 2>&1
+echo "deb https://download.mono-project.com/repo/ubuntu stable-focal main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list >> $LOG_PATH/packages.log 2>&1
 sudo apt -y update >> $LOG_PATH/packages.log 2>&1
 sudo apt -y install mono-complete >> $LOG_PATH/packages.log 2>&1
