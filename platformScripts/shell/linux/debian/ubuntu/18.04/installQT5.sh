0bfbe5e2a93039ae36093c54bea4ce9b4a0a3329
Julianus Pfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2022-10-12 17:48:27 +0200

    add publicly visible sqlite to qt installation

--- platformScripts/shell/linux/debian/ubuntu/18.04/installQT5.sh
+++ platformScripts/shell/linux/debian/ubuntu/18.04/installQT5.sh
@@ -1,1 +1,3 @@
-sudo apt-get -y install qtbase5-dev libqt5svg5-dev >> $LOG_PATH/packages.log 2>&1
+# install sqlite, too, to make it publicly findable. Otherwise Qt just uses a hidden dynamic sqlite lib
+# and we dont find it. We cannot use the one from contrib, otherwise the programs crash. Due to symbol/implementation mismatches
+sudo apt-get -y install qtbase5-dev libqt5svg5-dev libsqlite3-dev libqt5sql5-sqlite >> $LOG_PATH/packages.log 2>&1
