b279a2e502f977cc20db4fb6deabcddbbba51b02
Julianus Pfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2022-11-23 16:18:10 +0100

    update cmake

--- platformScripts/shell/linux/debian/setup_and_test_openms.sh
+++ platformScripts/shell/linux/debian/setup_and_test_openms.sh
@@ -51,9 +51,9 @@ tick
 sudo apt-get -y install make >> $LOG_PATH/packages.log 2>&1
 
 # Install a more recent version of CMake to test some 3.1+ features for e.g. coverage tests
-curl -sSL -O https://cmake.org/files/v3.6/cmake-3.6.2-Linux-x86_64.tar.gz 
-sudo tar zxf cmake-3.6.2-Linux-x86_64.tar.gz -C /opt/
-export PATH="/opt/cmake-3.6.2-Linux-x86_64/bin:$PATH"
+curl -sSL -O https://github.com/Kitware/CMake/releases/download/v3.25.0/cmake-3.25.0.tar.gz
+sudo tar zxf cmake-3.25.0.tar.gz -C /opt/
+export PATH="/opt/cmake-3.25.0/bin:$PATH"
 
 echo "Setting up OpenMS core build tools took (s):"
 tock
